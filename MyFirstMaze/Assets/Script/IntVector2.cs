990f7cbe837eee0d338f43f6e6a9b1c9610517d5
Chewy12 <leech5@bxscience.edu>
2016-10-26 10:04:05 -0400

    Add files via upload

---
+++ MyFirstMaze/Assets/Script/IntVector2.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntVector2 : MonoBehaviour {
+    public struct IntVector2
+    {
+        public int x, z;
+        public IntVector2(int x, int z)
+        {
+            this.x = x;
+            this.z = z;
+        }
+
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
